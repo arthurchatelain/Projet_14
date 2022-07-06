deea2d00d340419e9e00e273f24f20210f79bec2
Arthur Chatelain <arthur.chatelain.off@gmail.com>
2022-07-06 10:17:20 +0200

    trim

--- src/lib/employee.js
+++ src/lib/employee.js
@@ -13,10 +13,10 @@ const validChaine = (chaine) => {
 }
 
 export async function isValid (firstName = '', lastName = '', street = '', city = '', state = '', zipCode = '', department = '') {
-        if (firstName.length <= 2 || firstName[0] === ' ' || validChaine(firstName) === false) throw new Error('invalid firstName')
-        else if (lastName.length <= 2 || lastName[0] === ' ' || validChaine(lastName) === false) throw new Error('invalid lastName')
-        else if (street.length <= 3 || street[0] === ' ' || validChaine(street) === false) throw new Error('invalid street')
-        else if (city.length <= 3 || city[0] === ' ' || validChaine(city) === false) throw new Error('invalid city')
+        if (firstName.length <= 2 || validChaine(firstName) === false) throw new Error('invalid firstName')
+        else if (lastName.length <= 2 || validChaine(lastName) === false) throw new Error('invalid lastName')
+        else if (street.length <= 3 || validChaine(street) === false) throw new Error('invalid street')
+        else if (city.length <= 3 || validChaine(city) === false) throw new Error('invalid city')
         else if (zipCode.match(/^([0-9]){1,}$/) === null) throw new Error('invalid zipCode')
         else if (departmentList.find(item => item === department) === undefined) throw new Error('please select a valid department')
         else if (states.find(item => item === state) === undefined) throw new Error('please select a valid state')
