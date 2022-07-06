40064cbb8679255f7abb7f30fdde379c58fd7596
Arthur Chatelain <arthur.chatelain.off@gmail.com>
2022-07-06 10:06:07 +0200

    no weirds caract

--- src/lib/employee.js
+++ src/lib/employee.js
@@ -2,11 +2,21 @@ import { states, departmentList } from "../Assets/Data/States"
 
 // Tests if a typed data are valid
 
+const validChaine = (chaine) => {
+    let value = true
+    Array.from(chaine).forEach(item => {
+        Array.from('&!§:/;.%*µ$£@|(){}=+#~').forEach(i => {
+            if (i === item) value = false
+        })
+    })
+    return value
+}
+
 export async function isValid (firstName = '', lastName = '', street = '', city = '', state = '', zipCode = '', department = '') {
-        if (firstName.length <= 2 || firstName[0] === ' ') throw new Error('invalid firstName')
-        else if (lastName.length <= 2 || lastName[0] === ' ') throw new Error('invalid lastName')
-        else if (street.length <= 3 || street[0] === ' ') throw new Error('invalid street')
-        else if (city.length <= 3 || city[0] === ' ') throw new Error('invalid city')
+        if (firstName.length <= 2 || firstName[0] === ' ' || validChaine(firstName) === false) throw new Error('invalid firstName')
+        else if (lastName.length <= 2 || lastName[0] === ' ' || validChaine(lastName) === false) throw new Error('invalid lastName')
+        else if (street.length <= 3 || street[0] === ' ' || validChaine(street) === false) throw new Error('invalid street')
+        else if (city.length <= 3 || city[0] === ' ' || validChaine(city) === false) throw new Error('invalid city')
         else if (zipCode.match(/^([0-9]){1,}$/) === null) throw new Error('invalid zipCode')
         else if (departmentList.find(item => item === department) === undefined) throw new Error('please select a valid department')
         else if (states.find(item => item === state) === undefined) throw new Error('please select a valid state')
